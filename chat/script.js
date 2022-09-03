1978637b3229c93111e88d3637df8af957fc0630
SeniorJack <45121264+ob-cell@users.noreply.github.com>
2022-09-04 00:56:07 +0100

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -35,3 +35,40 @@ fetchChat.on("child_added", function (snapshot) {
   const msg = "<li>" + messages.usr + " : " + messages.msg + "</li>";
   document.getElementById("messages").innerHTML += msg;
 });
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
